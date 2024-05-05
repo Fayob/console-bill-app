33a7b278721cf04d1bb1ddbc8b73ccc9aecae019
fayob <adedejifavour6@gmail.com>
2024-05-05 13:46:26 +0100

    Add constructor and get input from user functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use std::{collections::HashMap, io};
+
+#[derive(Debug, Clone)]
+pub struct Bill {
+    name: String,
+    amount: f64,
+}
+
+pub struct Bills {
+    inner: HashMap<String, Bill>
+}
+
+impl Bills {
+    fn new() -> Self {
+        Self {
+            inner: HashMap::new(),
+        }
+    }
+}
+
+fn get_input() -> Option<String> {
+    let mut buffer = String::new();
+    while io::stdin().read_line(&mut buffer).is_err() {
+        println!("Please enter your data again");
+    }
+    let input = buffer.trim().to_owned();
+    if &input == "" {
+        None
+    } else {
+        Some(input)
+    }
+}
+
+
 fn main () {
-    println!("Hello World")
+    let mut bills = Bills::new();
 }
