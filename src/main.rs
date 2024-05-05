7da95d7e8f62eec91bda0fb380b3fffed19c3bc4
fayob <adedejifavour6@gmail.com>
2024-05-05 13:50:14 +0100

    Add functionality to add bills

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,10 @@ impl Bills {
             inner: HashMap::new(),
         }
     }
+
+    fn add(&mut self, bill: Bill){
+        self.inner.insert(bill.name.to_string(), bill);
+    }
 }
 
 fn get_input() -> Option<String> {
@@ -31,7 +35,73 @@ fn get_input() -> Option<String> {
     }
 }
 
+fn get_bill_amount() -> Option<f64> {
+    println!("Amount");
+    loop {
+        let input = match get_input() {
+            Some(input) => input,
+            None => return None,
+        };
+        if &input == "" {
+            return  None;
+        }
+        let parsed_input:  Result<f64, _> = input.parse();
+        match parsed_input {
+            Ok(amount) => return Some(amount),
+            Err(_) => println!("Please enter a number"),
+        }
+    }
+}
+
+mod menu {
+    use crate::{ get_bill_amount, get_input, Bill, Bills };
+
+    pub fn add_bill(bills: &mut Bills) {
+        println!("Bill name: ");
+        let name = match get_input() {
+            Some(input) => input,
+            None => return,
+        };
+        let amount = match get_bill_amount() {
+            Some(amt) => amt,
+            None => return,
+        };
+        let bill = Bill { name, amount };
+        bills.add(bill);
+        println!("Bill added");
+    }
+}
+
+enum MainMenu {
+    AddBill,
+}
+
+impl MainMenu {
+    fn from_str(input: &str) -> Option<MainMenu> {
+        match input {
+            "1" => Some(Self::AddBill),
+            _ => None,
+        }
+    }
+
+    fn show() {
+        println!("");
+        println!(" == Bill Manager ==");
+        println!("1. Add Bill");
+        println!("");
+        print!("Enter selection: ");
+    }
+}
 
 fn main () {
     let mut bills = Bills::new();
+
+    loop {
+        MainMenu::show();
+        let input = get_input().expect("no data entered");
+        match MainMenu::from_str(&input){
+            Some(MainMenu::AddBill) => menu::add_bill(&mut bills),
+            None => return,
+        }
+    }
 }
